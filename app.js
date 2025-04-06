48db74fe87468ee4e2bcd3b60a2fb5518bbf1db9
ahmed101essam <ahmed10pepeto@gmail.com>
2025-04-07 01:12:08 +0200

    editing get project

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(
-  cors(
-    origin:"*"
-  )
+  cors({
+    origin: "*",
+  })
   // cors({
   //   origin: "http://localhost:5173",
   //   credentials: true,
